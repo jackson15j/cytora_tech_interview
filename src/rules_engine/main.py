6cf332aa9590b016af7daa4aff9ed7beb7a6102f
Craig Astill <jackson15j@hotmail.com>
2024-04-08 11:29:30 +0100

    chore: Print evaluation in `main()`.

--- src/rules_engine/main.py
+++ src/rules_engine/main.py
@@ -49,9 +49,40 @@ class Or:
 def evaluate(rules, data: dict = {}) -> bool:
     return rules.execute(data)
 
+RULE_TEXT = """
+Either:
+  credit_rating is above 50
+  AND
+  flood_risk is below 10
+OR
+  revenue is above 1000000
+"""
+RULE_1 = Or(
+    (
+        And(
+            (
+                Rule("credit_rating", 50, Operator.greater_than),  # True
+                Rule("flood_risk", 10, Operator.less_than),  # True
+            )
+        ),  # True
+        Rule("revenue", 1000000, Operator.greater_than),  # False
+    )
+)  # True
+
+EXAMPLE_1 = {
+    "credit_rating": 75,
+    "flood_risk": 5,
+    "revenue": 1000
+}
+EXAMPLE_1_EXP = True
 
 def main():
-    print("hello world")
+    print(f"Rule:\n{RULE_TEXT!r}\n\nIs represented as:\n\n{RULE_1!r}\n")
+    print(
+        f"Using the following data, should evaluate as: "
+        f"{EXAMPLE_1_EXP!r}\n\n{EXAMPLE_1!r}\n"
+    )
+    print("Evaluation:", evaluate(RULE_1, EXAMPLE_1))
 
 
 if __name__ == '__main__':
