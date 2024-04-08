008515d9daadc5b4779413e859b4f8ca5b143a1e
Craig Astill <jackson15j@hotmail.com>
2024-04-08 11:30:02 +0100

    docs: typing updates + retrospective.

--- src/rules_engine/main.py
+++ src/rules_engine/main.py
@@ -5,6 +5,7 @@ from enum import Enum
 
 
 class Operator(Enum):
+    """Enum for comparison operator."""
     less_than = operator.lt
     less_than_equal = operator.le
     greater_than = operator.gt
@@ -16,7 +17,7 @@ class Operator(Enum):
 @dataclass
 class Rule:
     """Comparison rule instance."""
-    a: int
+    a: int|str
     b: int
     operator: Operator
 
@@ -24,6 +25,9 @@ class Rule:
         if data:
             # Assuming `self.a` is the dict Key instead of the value,
             # which I was initially using in early testing.
+
+            # Retrospective: Handle KeyError gracefully either here or
+            # upstream.
             self.a = data[self.a]
         return self.operator.value(self.a, self.b)
 
@@ -31,18 +35,22 @@ class Rule:
 @dataclass
 class And:
     """And together the results of all executed rules."""
-    rules: list  # list['And'|'Or'|Rule]|tuple['And'|'Or'|Rule]
+    rules: list|tuple  # list['And'|'Or'|Rule]|tuple['And'|'Or'|Rule]
 
     def execute(self, data: dict = {}) -> bool:
+        # Retrospective: Break out loop to add error
+        # handling/reporting.
         return all(x.execute(data) for x in self.rules)
 
 
 @dataclass
 class Or:
     """Or together the results of all executed rules."""
-    rules: list  # list[And|'Or'|Rule]|tuple[And|'Or'|Rule]
+    rules: list|tuple  # list[And|'Or'|Rule]|tuple[And|'Or'|Rule]
 
     def execute(self, data: dict = {}) -> bool:
+        # Retrospective: Break out loop to add error
+        # handling/reporting.
         return any(x.execute(data) for x in self.rules)
 
 
